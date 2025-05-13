9d8874c57cf589f077af81b93bba0a9e409910f7
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-05-13 21:17:21 +0600

    i have initialized here mongoDB and hide thepasswords

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express');
-const app=express();
+const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
 
 // MiddleWare
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-res.send('Hello Boss This is Espresso Emporium Website Server')
+
+// Mongo DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r0dgoug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Boss! This is Espresso Emporium Website Server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Espresso Emporium Server is Running On Port ${port}`)
 })
